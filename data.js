fce0d7a73d8f4bdad24036d4e00ffbd6a668bd92
Deisy Moreno <52799752+deisymorlop1103@users.noreply.github.com>
2023-10-31 11:48:31 -0500

    Update data.js
    add url image

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ const data = [
     "price": 109.95,
     "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
     "category": "men's clothing",
-    "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    "image": "https://i.pinimg.com/564x/1e/35/d6/1e35d678e14094becdfb25266b296be7.jpg",
     "rating": {
       "rate": 3.9,
       "count": 120
