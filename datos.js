100c7afa39a1123bf98ba9b03f8c71f91d8ad40a
Deisy Moreno <52799752+deisymorlop1103@users.noreply.github.com>
2023-11-05 12:03:52 -0500

    Create datos.js
    add class datos.js

---
+++ datos.js
@@ -0,0 +1,21 @@
+class Datos {
+  constructor() {
+    this.usuarios = JSON.parse(localStorage.getItem("usuarios"));
+  }
+
+  login(username, password) {
+    if (this.usuarios.find(usuario => usuario.username === username && usuario.password === password)) {
+      localStorage.setItem("usuario", username);
+      window.location.href = "home.html";
+    } else {
+      alert("El nombre de usuario o la contraseña son incorrectos.");
+    }
+  }
+
+  register(username, password) {
+    this.usuarios.push({
+      username,
+      password
+    });
+
+    localStorage.setItem("usuarios", JSON
