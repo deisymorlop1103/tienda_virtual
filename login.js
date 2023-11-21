5ebbd233e68ec49592f8c24260514de790df6e49
Deisy Moreno <52799752+deisymorlop1103@users.noreply.github.com>
2023-11-20 21:42:07 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,65 +1,12 @@
-// Importamos la librería de JQuery
-const $ = require("jquery");
+import firebase from 'firebase';
 
-// Creamos un arreglo de objetos JSON para almacenar los datos de los usuarios
-let dataLogin = [];
+const firebaseConfig = {
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  appId: 'YOUR_APP_ID',
+};
 
-// Función para iniciar sesión
-function iniciarSesion() {
-  // Obtenemos los datos del formulario
-  const usuario = $("#usuario").val();
-  const contraseña = $("#contraseña").val();
-
-  // Recorremos el arreglo de usuarios para encontrar una coincidencia
-  for (let i = 0; i < dataLogin.length; i++) {
-    // Si encontramos una coincidencia, iniciamos sesión
-    if (dataLogin[i].usuario === usuario && dataLogin[i].contraseña === contraseña) {
-      // Guardamos el nombre de usuario en local storage
-      localStorage.setItem("usuario", usuario);
-
-      // Redirigimos a la página principal
-      window.location.href = "index.html";
-      return;
-    }
-  }
-
-  // Si no encontramos una coincidencia, mostramos un mensaje de error
-  alert("Usuario o contraseña incorrectos");
-}
-
-// Función para registrar un nuevo usuario
-function registrarUsuario() {
-  // Obtenemos los datos del formulario
-  const usuario = $("#usuario").val();
-  const contraseña = $("#contraseña").val();
-
-  // Creamos un nuevo objeto JSON con los datos del usuario
-  const nuevoUsuario = {
-    usuario,
-    contraseña,
-  };
-
-  // Agregamos el nuevo usuario al arreglo de usuarios
-  dataLogin.push(nuevoUsuario);
-
-  // Guardamos los datos de los usuarios en local storage
-  localStorage.setItem("dataLogin", JSON.stringify(dataLogin));
-
-  // Redirigimos a la página de login
-  window.location.href = "login.html";
-}
-
-// Eventos
-
-// Cuando se presiona el botón de iniciar sesión, llamamos a la función iniciarSesion()
-$("#login").on("click", iniciarSesion);
-
-// Cuando se presiona el botón de registrar, llamamos a la función registrarUsuario()
-$("#registrar").on("click", registrarUsuario);
-
-// Al cargar la página, verificamos si hay un usuario registrado en local storage
-if (localStorage.getItem("dataLogin")) {
-  // Si hay un usuario registrado, iniciamos sesión
-  const datosLogin = JSON.parse(localStorage.getItem("dataLogin"));
-  iniciarSesion(datosLogin[0].usuario, datosLogin[0].contraseña);
-}
+firebase.initializeApp(firebaseConfig);
