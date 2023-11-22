7ab18489f0d8251427b1a3347f8fbc8f0eccf775
Deisy Moreno <52799752+deisymorlop1103@users.noreply.github.com>
2023-11-22 11:24:45 -0500

    Create Usuarios.js

---
+++ Usuarios.js
@@ -0,0 +1,62 @@
+// Clase para almacenar los usuarios registrados
+class Usuarios {
+  constructor() {
+    this.usuarios = [];
+  }
+
+  // Añade un nuevo usuario a la lista
+  registrarUsuario(nombre, correo, contrasena) {
+    const usuario = {
+      nombre,
+      correo,
+      contrasena,
+    };
+
+    this.usuarios.push(usuario);
+
+    // Convertimos los datos a JSON
+    const usuariosJson = JSON.stringify(this.usuarios);
+
+    // Almacenamos los datos en el localStorage
+    localStorage.setItem("usuarios", usuariosJson);
+  }
+
+  // Comprueba si el usuario existe
+  existeUsuario(correo) {
+    return this.usuarios.some((usuario) => usuario.correo === correo);
+  }
+
+  // Comprueba si las credenciales son correctas
+  credencialesCorrectas(correo, contrasena) {
+    return this.existeUsuario(correo) && this.usuarios.find((usuario) => usuario.correo === correo).contrasena === contrasena;
+  }
+}
+
+// Instancia de la clase Usuarios
+const usuarios = new Usuarios();
+
+// Registro de usuario
+document.querySelector(".registro form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const nombre = document.querySelector(".registro input[name='nombre']").value;
+  const correo = document.querySelector(".registro input[name='correo']").value;
+  const contrasena = document.querySelector(".registro input[name='contrasena']").value;
+
+  usuarios.registrarUsuario(nombre, correo, contrasena);
+});
+
+// Inicio de sesión
+document.querySelector(".login form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const correo = document.querySelector(".login input[name='correo']").value;
+  const contrasena = document.querySelector(".login input[name='contrasena']").value;
+
+  const inicioSesionExitosa = usuarios.credencialesCorrectas(correo, contrasena);
+
+  if (inicioSesionExitosa) {
+    // Redirigir a la página principal
+    window.location.href = "/";
+  }
+});
