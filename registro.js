60d35eb5028d0d8fff186cb0224f83aabf353a4f
Deisy Moreno <52799752+deisymorlop1103@users.noreply.github.com>
2023-11-21 21:24:11 -0500

    Update registro.js

--- registro.js
+++ registro.js
@@ -1,21 +1,65 @@
-function registrarUsuario() {
-  // Obtenemos los datos del formulario
-  const nombre = document.querySelector("input[name='nombre']").value;
-  const correo = document.querySelector("input[name='correo']").value;
-  const contraseña = document.querySelector("input[name='contraseña']").value;
+class login {
+  constructor() {
+    this.usuarios = [];
+  }
 
-  // Convertimos los datos a JSON
-  const usuario = {
-    nombre,
-    correo,
-    contraseña,
-  };
-  // Almacenamos los datos en el local storage
-  localStorage.setItem("usuario", JSON.stringify(usuario));
+  registrarUsuario(nombre, correo, contrasena) {
+    const usuario = {
+      nombre,
+      correo,
+      contrasena,
+    };
 
-  // Redirigimos a la página principal
-  window.location.href = "index.html";
+    this.usuarios.push(usuario);
+
+    // Convertimos los datos a JSON
+    const usuariosJson = JSON.stringify(this.usuarios);
+
+    // Almacenamos los datos en el localStorage
+    localStorage.setItem("usuarios", usuariosJson);
+  }
+
+  iniciarSesion(correo, contrasena) {
+    // Recorremos los usuarios registrados
+    for (const usuario of this.usuarios) {
+      // Si el correo y la contraseña coinciden
+      if (usuario.correo === correo && usuario.contrasena === contrasena) {
+        // Sesión iniciada
+        console.log("Sesión iniciada");
+        return true;
+      }
+    }
+
+    // Sesión no iniciada
+    console.log("Sesión no iniciada");
+    return false;
+  }
 }
 
-// Evento del botón de envío
-document.querySelector("input[type='submit']").addEventListener("click", registrarUsuario);
+const login = new Login();
+
+// Registro de usuario
+document.querySelector(".registro form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const nombre = document.querySelector(".registro input[name='nombre']").value;
+  const correo = document.querySelector(".registro input[name='correo']").value;
+  const contrasena = document.querySelector(".registro input[name='contrasena']").value;
+
+  login.registrarUsuario(nombre, correo, contrasena);
+});
+
+// Inicio de sesión
+document.querySelector(".login form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const correo = document.querySelector(".login input[name='correo']").value;
+  const contrasena = document.querySelector(".login input[name='contrasena']").value;
+
+  const inicioSesionExitosa = login.iniciarSesion(correo, contrasena);
+
+  if (inicioSesionExitosa) {
+    // Redirigir a la página principal
+    window.location.href = "index.html";
+  }
+});
